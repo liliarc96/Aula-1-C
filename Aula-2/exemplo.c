8ebef3353e6fbad1e4e1a2e4efe5d6446b85305a
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-02-12 22:17:37 -0200

    Create exemplo.c

---
+++ Aula-2/exemplo.c
@@ -0,0 +1,14 @@
+#include <stdio.h> //Inclusão de bibliotecas
+
+void olaMundo();
+
+int main(){
+  float num1; //Variáveis locais
+  
+  olaMundo(); //Chamada de funções
+  return 0;
+}
+
+void olaMundo(){
+  printf("%f", "Olá Mundo");
+}
