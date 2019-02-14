f622f0a5c3350c3116c68d032bcfe513d8cf8f11
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-02-14 21:36:29 -0200

    Update exemplo.c

--- Aula-2/exemplo.c
+++ Aula-2/exemplo.c
@@ -1,7 +1,12 @@
 #include <stdio.h> //Inclusão de bibliotecas
 
 void olaMundo();
-
+//A função olaMundo não funciona sem o void olaMundo(); se não for posicionada antes do main.
+/*
+void olaMundo(){
+  printf("%f", "Olá Mundo");
+}
+*/
 int main(){
   float num1; //Variáveis locais
   
