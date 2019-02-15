80515caa2bf552756e6e04a5c072a9ddd578d295
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-02-14 22:12:50 -0200

    Create array.c

---
+++ Aula-3/array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int array_de_inteiro [10] = {1,2,3,4,5,6,7,8,9,10};
+
+	//Não instancia o array todo:
+	//int array_errado [10] = {1,2,3};
+
+	printf("O elemento do array é: %d\n", array_de_inteiro[4]);
+  return 0;
+}
