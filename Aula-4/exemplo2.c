4da5af754a552e374b059e90c0a7697d97446502
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-02-19 21:08:19 -0300

    Update exemplo2.c

--- Aula-4/exemplo2.c
+++ Aula-4/exemplo2.c
@@ -8,9 +8,9 @@ int main(void) {
 	unsigned char d;
 	unsigned char e;
 
-	c = (a == b);
-	d = (a == c);
-	e = (a == c ||  a == b);
+	c = (a == b); // c recebe o valor de (a == b), o valor de a é 10 e o de b também é 10, o que significa que c recebeu true (c = 1)
+	d = (a == c); // d recebe o valor de (a == c), o valor de a é 10 e o de c é 1, o que significa que d recebeu false (d = 0)
+	e = (a == c ||  a == b); // e recebe o valor de (a == c ||  a == b), ou seja (0 || 1) = true, significa que e recebeu true (e = 1)
 
 	printf("O valor de C é: %d\n", c);
 	printf("O valor de D é: %d\n", d);
