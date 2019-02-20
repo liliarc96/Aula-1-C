11c7ad89bb3f1444c299a149803741d226daee4a
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-02-19 21:50:32 -0300

    Create ternario_exemplo.c

---
+++ Aula-4/ternario_exemplo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int a, b;
+/*
+  if(a == b){
+    printf("A é igual a B");
+  }
+  else{
+    printf("A é diferente de B");
+  }
+
+condicao_logica ? expressa_se_verdadeira : expressao_se_falsa
+Exemplo: x = (a == b)?(a):(b);
+  a = 2;
+  b = 2;
+*/
+  (a == b)?printf("A é igual a B"):printf("A é diferente de B");
+
+	return 0;
+}
