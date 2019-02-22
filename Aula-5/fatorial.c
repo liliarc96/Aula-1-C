ba08b9ceb2d4d502d16555c95e6836015109e41b
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-02-21 21:28:12 -0300

    Create fatorial.c

---
+++ Aula-5/fatorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+  int numero, contador, fatorial;
+  printf("\nDigite um numero inteiro maior que zero: ");
+  scanf("%d", &numero);
+  fatorial = numero;
+
+  if(numero > 0){
+    for(contador = 1; numero > contador ;contador++){
+        fatorial = fatorial*(numero-contador);
+      }
+    printf("\n!%d é igual a: %d", numero, fatorial);
+  }
+  else{
+    printf("ERRO.");
+  }
+  return 0;
+}
